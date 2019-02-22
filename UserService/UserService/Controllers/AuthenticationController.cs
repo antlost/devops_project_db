2ca99ee2c54390cded9d031fed88b6d2f633a981
Anthony Losty <anthony.losty@version1.com>
2019-02-22 15:39:46 +0000

    UserService added

---
+++ UserService/UserService/Controllers/AuthenticationController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UserService.Model;
+
+namespace UserService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        //test variables
+        Registration TestUser = new Registration { FirstName = "Conor", LastName = "Oneill", Email = "test@version1.com", Password = "test123" };
+
+
+
+        //// GET api/values
+        //[HttpGet]
+        //public ActionResult<IEnumerable<string>> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        // POST api/Authentication
+        [HttpPost]
+        public object Post(string email, string password)
+        {
+            if (TestUser.Email == email && TestUser.Password == password)
+            {
+                return this.Ok("success");
+            }
+            else
+            {
+                return this.Unauthorized();
+            }
+        }
+    }
+}
